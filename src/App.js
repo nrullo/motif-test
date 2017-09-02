3d9376214d728a409f3a9910dea73b4115cac5c3
Nahuel Rullo <nahuelrullo@gmail.com>
2017-09-02 10:37:55 -0300

    add http request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import rp from 'request-promise';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleOnClick=this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(){
+    rp({
+      method: 'GET',
+      uri: 'https://jsonplaceholder.typicode.com/posts/1'
+    })
+    .then(response=>{
+      console.log('response:', response);
+    })
+    .catch(e=>{
+      console.error('error:', e);
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +33,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.handleOnClick}>Make HTTP request</button>
       </div>
     );
   }
